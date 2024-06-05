da6bd4a4b0781b0a33ea4cf24b9f38cc66d1c6d2
Grant Forsythe <grantwforsythe@gmail.com>
2024-06-04 23:27:23 -0400

    feat: add autoclose and auto rename tag

--- lua/main/plugins/autoclose.lua
+++ lua/main/plugins/autoclose.lua
@@ -1,6 +1,11 @@
-return {
+return {{
     'm4xshen/autoclose.nvim',
     config = function()
         require('autoclose').setup({})
     end
-}
+},{
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup({})
+        end
+}}
