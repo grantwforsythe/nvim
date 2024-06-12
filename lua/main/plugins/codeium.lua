99727438e3308756cbe00a632e43b9c4bc80bb81
Grant Forsythe <grantwforsythe@gmail.com>
2024-06-11 21:18:20 -0400

    fix: remove tab completion for codeium

--- lua/main/plugins/codeium.lua
+++ lua/main/plugins/codeium.lua
@@ -4,6 +4,7 @@ return {
     version = '1.8.37',
     event = 'BufEnter',
     config = function()
+        vim.g.codeium_disable_no_map_tab = 1
         vim.keymap.set('i', '<C-g>', function()
             return vim.fn['codeium#Accept']()
         end, { expr = true, silent = true})
