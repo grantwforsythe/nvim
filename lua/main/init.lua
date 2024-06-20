f79d621958b05273d4900ca04f21f6884126a67a
Grant Forsythe <grantwforsythe@gmail.com>
2024-06-20 11:59:36 -0400

    refactor: create global folder

--- lua/main/init.lua
+++ lua/main/init.lua
@@ -1,7 +1,21 @@
-require('main.set')
--- Need to call remap first as it sets leader before loading plugins
-require('main.remap')
-require('main.lazy_init')
+require('main.global.set')
+require('main.global.remap')
+
+-- Install lazy if it is not already installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system(
+        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+         lazypath})
+end
+vim.opt.rtp:prepend(lazypath) -- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+require('lazy').setup({
+    spec = 'main.plugins', -- Import all plugins
+    ui = {
+        border = 'rounded'
+    }
+})
 
 vim.o.background = 'dark'
 vim.cmd([[colorscheme gruvbox]])
