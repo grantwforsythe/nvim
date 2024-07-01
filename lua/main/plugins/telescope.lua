53f3fdea7613fe32db3ad134a36f8470e1fd67aa
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-01 19:11:02 -0400

    feat: add mappings for jumplist and buffers

--- lua/main/plugins/telescope.lua
+++ lua/main/plugins/telescope.lua
@@ -5,7 +5,21 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
       local builtin = require('telescope.builtin')
+      local actions = require('telescope.actions')
+
+      require('telescope').setup{
+          defaults = {
+              mappings = {
+                  i = {
+                      ["<esc>"] = actions.close
+                  }
+              }
+          }
+      }
+
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>pj', builtin.jumplist, {})
       vim.keymap.set('n', '<C-p>', function()
             builtin.git_files({ recurse_submodules = true})
       end, {})
