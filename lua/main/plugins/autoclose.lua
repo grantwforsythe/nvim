778a8a10460a0752b7db787304e268800c77c4b9
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-02 08:42:15 -0400

    feat: disable autoclose in text and markdown

--- lua/main/plugins/autoclose.lua
+++ lua/main/plugins/autoclose.lua
@@ -1,11 +1,18 @@
-return {{
+return {
+    {
     'm4xshen/autoclose.nvim',
     config = function()
-        require('autoclose').setup({})
+        require('autoclose').setup({
+            options = {
+                disabled_filetypes = { 'text', 'markdown' }
+            }
+        })
     end
-},{
+    },
+    {
         'windwp/nvim-ts-autotag',
         config = function()
             require('nvim-ts-autotag').setup({})
         end
-}}
+    }
+}
