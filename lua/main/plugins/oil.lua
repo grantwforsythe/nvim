9b267dd394de141b1cabce9553ecca16a1bb6f83
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-02 23:49:05 -0400

    feat: add some more oil settings

--- lua/main/plugins/oil.lua
+++ lua/main/plugins/oil.lua
@@ -4,15 +4,19 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     require("oil").setup({
       default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
       keymaps = {
         -- Disable because it conflicts with telescope
         ["<C-p>"] = false,
       },
       view_options = {
-        show_hidden = true
+        show_hidden = true,
+        is_always_hidden = function(name, _)
+          return name == ".git" or name == ".."
+        end
       }
     })
   end
