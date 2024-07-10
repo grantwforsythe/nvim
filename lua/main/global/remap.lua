915fbc24d4dc7960148c1c84dd51cf23287b515e
Grant Forsythe <23422098+grantwforsythe@users.noreply.github.com>
2024-07-10 13:13:02 -0400

    feat: add window navigation remaps

--- lua/main/global/remap.lua
+++ lua/main/global/remap.lua
@@ -39,3 +39,19 @@ vim.keymap.set(
 )
 
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { desc = 'Make current file executable', silent = true })
+
+-- Window navigation
+vim.keymap.set('t', '<A-h>', [[<C-\><C-N><C-w>h]])
+vim.keymap.set('t', '<A-j>', [[<C-\><C-N><C-w>j]])
+vim.keymap.set('t', '<A-k>', [[<C-\><C-N><C-w>k]])
+vim.keymap.set('t', '<A-l>', [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set('i', '<A-h>', [[<C-\><C-N><C-w>h]])
+vim.keymap.set('i', '<A-j>', [[<C-\><C-N><C-w>j]])
+vim.keymap.set('i', '<A-k>', [[<C-\><C-N><C-w>k]])
+vim.keymap.set('i', '<A-l>', [[<C-\><C-N><C-w>l]])
+
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
