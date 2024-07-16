4482c3df7dae6d3da5db297816f56f4dbf205d13
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-16 18:00:50 -0400

    feat: disable autoclose for string characters

--- lua/main/plugins/autoclose.lua
+++ lua/main/plugins/autoclose.lua
@@ -1,9 +1,14 @@
 return {
     {
-    'm4xshen/autoclose.nvim',
-    opts = {
+        'm4xshen/autoclose.nvim',
+        opts = {
+            keys = {
+                ['"'] = { escape = true, close = false, pair = '""' },
+                ["'"] = { escape = true, close = false, pair = "''" },
+            },
             options = {
-                disabled_filetypes = { 'text', 'markdown' }
+                disabled_filetypes = { 'text', 'markdown' },
+                disable_command_mode = true
             }
         }
     },
