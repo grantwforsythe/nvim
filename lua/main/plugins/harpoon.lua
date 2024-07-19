3821dc9a71e6f29b6ae95fdf69b7c3bfb00d3f33
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-19 17:11:50 -0400

    fix: remap keys for harpoon

--- lua/main/plugins/harpoon.lua
+++ lua/main/plugins/harpoon.lua
@@ -4,55 +4,34 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 	config = function()
 		local harpoon = require("harpoon")
+
 		harpoon:setup()
-
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
 
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
-
 		vim.keymap.set("n", "<C-e>", function()
-			toggle_telescope(harpoon:list())
-		end, { desc = "Open harpoon window" })
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-		-- TODO: Remap keys
-
-		vim.keymap.set("n", "<C-h>", function()
+		vim.keymap.set("n", "<leader>1", function()
 			harpoon:list():select(1)
 		end)
-		vim.keymap.set("n", "<C-t>", function()
+		vim.keymap.set("n", "<leader>2", function()
 			harpoon:list():select(2)
 		end)
-		vim.keymap.set("n", "<C-n>", function()
+		vim.keymap.set("n", "<leader>3", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<C-s>", function()
+		vim.keymap.set("n", "<leader>4", function()
 			harpoon:list():select(4)
 		end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function()
+		vim.keymap.set("n", "<C-P>", function()
 			harpoon:list():prev()
 		end)
-		vim.keymap.set("n", "<C-S-N>", function()
+		vim.keymap.set("n", "<C-N>", function()
 			harpoon:list():next()
 		end)
 	end,
