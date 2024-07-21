636825d63801f4a99ce3e2776fb71b42c4fa7461
Grant Forsythe <grantwforsythe@gmail.com>
2024-07-21 19:17:01 -0400

    feat<; add hop

---
+++ lua/main/plugins/hop.lua
@@ -0,0 +1,14 @@
+return {
+	"smoka7/hop.nvim",
+	version = "2*",
+	lazy = true,
+	opts = {
+		keys = "etovxqpdygfblzhckisuran",
+	},
+	keys = {
+		{ "<leader>w", "<cmd>HopWordAC<cr>", desc = "Hop Word after cursor" },
+		{ "<leader>W", "<cmd>HopWordBC<cr>", desc = "Hop Word before cursor" },
+		{ "<leader>f", "<cmd>HopPatternAC<cr>", desc = "Hop pattern after cursor" },
+		{ "<leader>F", "<cmd>HopPatternBC<cr>", desc = "Hop pattern before cursor" },
+	},
+}
