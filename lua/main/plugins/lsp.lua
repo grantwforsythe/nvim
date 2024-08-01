bb89cf648c4440ea506e830c1dfb529c26bf6426
Grant Forsythe <grantwforsythe@gmail.com>
2024-08-01 15:10:09 -0400

    feat: add icons to autocomplete

--- lua/main/plugins/lsp.lua
+++ lua/main/plugins/lsp.lua
@@ -14,10 +14,12 @@ return {
 		"hrsh7th/nvim-cmp",
 		lazy = false,
 		priority = 100,
-		dependencies = { "L3MON4D3/LuaSnip", "hrsh7th/cmp-cmdline", "hrsh7th/cmp-buffer" },
+		dependencies = { "L3MON4D3/LuaSnip", "hrsh7th/cmp-cmdline", "hrsh7th/cmp-buffer", "onsails/lspkind.nvim" },
 		config = function()
 			local lsp_zero = require("lsp-zero")
 			lsp_zero.extend_cmp()
+
+			local lspkind = require("lspkind")
 
 			-- auto completion
 			local cmp = require("cmp")
@@ -32,9 +34,12 @@ return {
 				completion = {
 					completeopt = "menu,menuone,noinsert",
 				},
-				formatting = lsp_zero.cmp_format({
-					details = true,
-				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-u>"] = cmp.mapping.scroll_docs(-4),
