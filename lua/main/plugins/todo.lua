ad0b305b74289ff9d8bbf637da9877a9fe3904d5
Grant Forsythe <grantwforsythe@gmail.com>
2024-08-25 20:21:28 -0400

    fix: add config key to todo

--- lua/main/plugins/todo.lua
+++ lua/main/plugins/todo.lua
@@ -1,20 +1,18 @@
 return {
 	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"]t",
-			function()
-				require("todo_comments").jump_next()
-			end,
-			desc = "Next todo comment",
-		},
-		{
-			"[t",
-			function()
-				require("todo_comments").jump_prev()
-			end,
-			desc = "Previous todo comment",
-		},
-	},
+	config = function()
+		local todo_comments = require("todo-comments")
+
+		vim.keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
+
+		vim.keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
+
+		todo_comments.setup()
+	end,
 }
