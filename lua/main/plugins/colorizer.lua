b74f8fd7268435bcd1a6d89a42a0fb883164f2aa
Grant Forsythe <grantwforsythe@gmail.com>
2024-09-05 08:32:59 -0400

    feat: add colorizer; rename plugin file

---
+++ lua/main/plugins/colorizer.lua
@@ -0,0 +1,12 @@
+return {
+	"norcalli/nvim-colorizer.lua",
+	keys = {
+		{
+			"<leader>c",
+			"<cmd>ColorizerToggle<CR>",
+			mode = "",
+			desc = "Toggle colorizer",
+		},
+	},
+	opts = {},
+}
--- lua/main/plugins/comments.lua
+++ lua/main/plugins/comments.lua
@@ -13,5 +13,4 @@ return {
 			pre_hook = ts_context_commentstring.create_pre_hook(),
 		})
 	end,
-	opts = {},
 }
