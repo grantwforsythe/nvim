c7908087482b8427a4da3f0989a0ef5ee7e157ce
Grant Forsythe <grantwforsythe@gmail.com>
2024-12-11 23:06:27 -0500

    feat: replace builting live_grep picker with own

--- lua/main/plugins/telescope.lua
+++ lua/main/plugins/telescope.lua
@@ -40,14 +40,75 @@ return {
 		{
 			"<leader>pg",
 			function()
-				require("telescope.builtin").live_grep()
+				local finders = require("telescope.finders")
+				local pickers = require("telescope.pickers")
+
+				local opts = {}
+				opts.cwd = vim.uv.cwd()
+
+				local finder = finders.new_async_job({
+					command_generator = function(prompt)
+						if not prompt or prompt == "" then
+							return nil
+						end
+
+						local pieces = vim.split(prompt, "  ")
+						-- ripgrep is being used
+						local args = { "rg" }
+
+						if pieces[1] then
+							table.insert(args, "-e")
+							table.insert(args, pieces[1])
+						end
+
+						-- File ext
+						if pieces[2] then
+							table.insert(args, "-g")
+							table.insert(args, pieces[2])
+						end
+
+						---@diagnostic disable-next-line: deprecated
+						return vim.tbl_flatten({
+							args,
+							{
+								"--color=never",
+								"--no-heading",
+								"--with-filename",
+								"--line-number",
+								"--column",
+								"--smart-case",
+							},
+						})
+					end,
+					entry_maker = require("telescope.make_entry").gen_from_vimgrep(opts),
+					cwd = opts.cwd,
+				})
+
+				local conf = require("telescope.config").values
+
+				pickers
+					.new(opts, {
+						debounce = 100,
+						prompt_title = "Live Grep",
+						finder = finder,
+						previewer = conf.grep_previewer(opts),
+						sorter = require("telescope.sorters").empty(),
+					})
+					:find()
 			end,
-			desc = "Query for text within all non-hidden git files using telescope",
+			desc = "Query for text within all non-hidden git files using telescope. Add two spaces to global by the file extension",
 		},
 		{
 			"<leader>pt",
 			"<cmd>TodoTelescope<CR>",
 			desc = "Query for text within all non-hidden git files using telescope",
+		},
+		{
+			"<leader>ph",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+			desc = "Query all help tags",
 		},
 		{
 			"<C-p>",
@@ -68,6 +129,9 @@ return {
 						["<esc>"] = actions.close,
 					},
 				},
+			},
+			extensions = {
+				fzf = {},
 			},
 		})
 
