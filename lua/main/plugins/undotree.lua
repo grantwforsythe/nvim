e33d4aa80d212c878dff7c4c01243ae876791c92
Grant Forsythe <grantwforsythe@gmail.com>
2025-01-09 21:40:38 -0500

    fix: undotree on windows

--- lua/main/plugins/undotree.lua
+++ lua/main/plugins/undotree.lua
@@ -3,4 +3,9 @@ return {
 	keys = {
 		{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Undotree" },
 	},
+	config = function()
+		if vim.loop.os_uname().sysname == "Windows_NT" then
+			vim.g.undotree_DiffCommand = "FC"
+		end
+	end,
 }
