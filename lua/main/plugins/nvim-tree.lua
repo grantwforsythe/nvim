eff6780326c903902f8952d36cdc25b40be67639
Grant Forsythe <grantwforsythe@gmail.com>
2025-01-28 08:08:01 -0500

    feat: add back nvim tree

---
+++ lua/main/plugins/nvim-tree.lua
@@ -0,0 +1,26 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{ "<leader>t", "<cmd>NvimTreeToggle<CR>", desc = "Toggle file tree" },
+	},
+	opts = {
+		sort = {
+			sorter = "case_sensitive",
+		},
+		view = {
+			width = 30,
+			side = "right",
+		},
+		renderer = {
+			group_empty = true,
+		},
+		filters = {
+			dotfiles = true,
+		},
+	},
+}
